3508946a79af8205ab7f2e280d77f1919bcf90b1
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2026-07-22 19:47:24 +0900

    Delete exercise directory

--- exercise/entity/ExerciseType.java
+++
@@ -1,7 +0,0 @@
-package Spring.FitLog.domain.exercise.entity;
-
-public enum ExerciseType {
-    WEIGHT,     // 웨이트 트레이닝
-    CARDIO,     // 유산소 운동
-    BODYWEIGHT  // 맨몸 운동
-}
--- exercise/repository/ExerciseRepository.java
+++
@@ -1,8 +0,0 @@
-package Spring.FitLog.domain.exercise.repository;
-
-import Spring.FitLog.domain.exercise.entity.Exercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
-    
-}
